07fd29fb1454055219f04f821b129a86db52837c
quangvinh090 <quangvinhnguyen4523@gmail.com>
2022-12-28 14:08:15 +0700

    Tuple

--- RequexTryParse_quangvinh/FuntionAndAction/Program.cs
+++ RequexTryParse_quangvinh/FuntionAndAction/Program.cs
@@ -1,6 +1,7 @@
 ﻿internal class Program
 {
- //   delegate void Display();
+    //   delegate void Display();
+    public static int Addd(int a, int b) => a - b;
     public static void Sub(int a, int b) => Console.WriteLine(a + b);
     public static void Show() => Console.WriteLine("C2109I1");
     public static int ReturnNumber() => 5;
@@ -16,5 +17,30 @@
    //     a();
         Action act = Show;
         act();
+
+        Action<int, int> ac = Sub;
+        ac(5,6);
+        //Action a = Add;  local function ko xai dc 
+        //a(5,6);
+
+
+        Func<int> fun = ReturnNumber;
+        Console.WriteLine(fun());
+
+        Func<int, int, int> fu = Addd;
+        Console.WriteLine(fu(10,5));
+
+        Action action = () =>
+        {
+            Console.WriteLine("ahihi");
+        };
+        action();
+
+        //Action string action = (str) =>
+        //{
+        //    Console.WriteLine(str);
+        //};
+        //action("hello");
+        //        action.Invoke();
     }
 }
---
+++ RequexTryParse_quangvinh/Tuple/Program.cs
@@ -0,0 +1,46 @@
+﻿using UsingTuple;
+
+
+// object In
+Person per = new()
+{
+    Id = 1,
+    FirstName = "truong",
+    LastName  = "nguyen"
+};
+
+Console.WriteLine(per.Id);
+Console.WriteLine(per.FirstName);
+
+//dung valuetuple khong can den class 
+
+ValueTuple<int, string, string> p1 = (1, "truong", "nguyen");
+Console.WriteLine(p1.Item1);
+Console.WriteLine(p1.Item2);
+
+//moi hon
+(int, string, string) p2 = (1, "truong", "nguyen");
+Console.WriteLine(p1.Item2);
+Console.WriteLine(p1.Item3);
+
+//dung var
+var p3 = (1, "quang", "vinh");
+
+var p4 = (1); //variable int p4 = 1
+
+
+//net cor dung name thay the cho cacc cai item1, item2
+(int id, string firstName, string lastName) p5 = (1, "quang", "vinh");
+Console.WriteLine(p5.firstName);
+Console.WriteLine(p5.lastName);
+
+//hoac dung name phia trc
+var p6 = (id: 1, firstName: "Quang", lastName: "Vinh");
+Console.WriteLine(p6.firstName);
+Console.WriteLine(p6.lastName);
+
+
+//khong phai laf tuple => anonymous type
+var p7 = new { id = 1, firstName = "Quang", lastname = "Vinh" };
+Console.WriteLine(p7.firstName);
+Console.WriteLine(p7.lastname);
